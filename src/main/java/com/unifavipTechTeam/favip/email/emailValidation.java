10f59b8a07b634b5f1c1367bfe1b26010fb6b540
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-29 21:42:53 -0300

    bugsFixed

--- src/main/java/com/unifavipTechTeam/favip/email/emailValidation.java
+++
@@ -1,4 +0,0 @@
-package com.unifavipTechTeam.favip.email;
-
-public @interface emailValidation {
-}
--- src/main/java/com/unifavipTechTeam/favip/entity/User.java
+++ src/main/java/com/unifavipTechTeam/favip/entity/User.java
@@ -11,7 +11,7 @@ import java.math.BigInteger;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public void setId(Long id) {
