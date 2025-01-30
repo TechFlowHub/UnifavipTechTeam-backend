0db5e4c7fb81fb252c40565f6fc5aa9e74b385e0
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-30 13:04:34 -0300

    addRequestHeaderAndResponseEntity

--- src/main/java/com/unifavipTechTeam/favip/email/EmailController.java
+++ src/main/java/com/unifavipTechTeam/favip/email/EmailController.java
@@ -1,21 +1,33 @@
 package com.unifavipTechTeam.favip.email;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("email")
 public class EmailController {
-    private SendEmailService sendEmailService;
+
+    private final SendEmailService sendEmailService;
+
+    @Value("${email.api.token}")
+    private String emailApiToken;
 
     public EmailController(SendEmailService sendEmailService) {
         this.sendEmailService = sendEmailService;
     }
 
     @PostMapping("/send")
-    public String sendEmail(@RequestBody EmailDto emailDto) {
-        sendEmailService.sendEmail(emailDto.to(), emailDto.subject(), emailDto.text()   );
-        return "Email sent";
+    public ResponseEntity<String> sendEmail(
+            @RequestHeader("X-Email-Token") String token,
+            @RequestBody EmailDto emailDto) {
+
+        if (!emailApiToken.equals(token)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Unauthorized: Invalid token");
+        }
+
+        sendEmailService.sendEmail(emailDto.to(), emailDto.subject(), emailDto.text());
+        return ResponseEntity.ok("Email sent");
     }
-
-
 }
